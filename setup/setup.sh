18ac1483e0bc3e042e83fcc86874c13a993cd032
Ben Hussey <ben@blip2.net>
2021-12-15 20:25:02 +0000

    added up script

--- setup/setup.sh
+++ setup/setup.sh
@@ -43,8 +43,7 @@ then
   apt-get --assume-yes install docker-ce docker-ce-cli containerd.io >>setup.log
 fi
 
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+pip3 install docker-compose >>setup.log
 adduser pi docker
 
 echo 'Copying configuration files...'
