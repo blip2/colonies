0ec4890fdfcb0e6cf6afe4062e076ac260e65ac0
Ben Hussey <ben@blip2.net>
2021-12-15 20:14:42 +0000

    pipe to setup.log

--- setup/setup.sh
+++ setup/setup.sh
@@ -36,11 +36,11 @@ fi
 
 if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get --assume-yes install ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  apt-get --assume-yes install ca-certificates curl gnupg lsb-release >>setup.log
+  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg >>setup.log
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update
-  apt-get --assume-yes install docker-ce docker-ce-cli containerd.io
+  apt-get update >>setup.log
+  apt-get --assume-yes install docker-ce docker-ce-cli containerd.io >>setup.log
 fi
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
