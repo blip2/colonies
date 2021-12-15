5f76b003aa82141b12975f3487c8dd28062b32c0
Ben Hussey <ben@blip2.net>
2021-12-15 20:19:44 +0000

    chmod docker-compose file

--- setup/setup.sh
+++ setup/setup.sh
@@ -44,6 +44,7 @@ then
 fi
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 adduser pi docker
 
 echo 'Copying configuration files...'
